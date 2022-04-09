bce4e556827a55e7db37e49d460b52268b97bd17
yuehao <879389355@qq.com>
2022-04-09 15:41:24 +0800

    feat: 引入sass

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig,normalizePath } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
+
+const variablePath=normalizePath(path.resolve('./src/variable.scss'))
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  css:{
+    preprocessorOptions:{
+      scss:{
+        additionalData:`@import "${variablePath}";`
+      }
+    }
+  }
 })
